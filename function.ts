66180c968f45492c4fa07a600ca40fdb7016790a
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-11-25 20:57:03 +0530

    typecript function syntax added

---
+++ function.ts
@@ -0,0 +1,10 @@
+function calculateArea(width: number, height: number): number {
+    return width * height;
+  }
+  
+  const rectangleWidth: number = 10;
+  const rectangleHeight: number = 5;
+  const area: number = calculateArea(rectangleWidth, rectangleHeight);
+  
+  console.log("Area:", area);
+  
