0f17e44a0bd6d62918037ef82f1b29e0f2918d67
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-12-14 20:48:45 +0530

    Update ifelse.ts

--- ifelse.ts
+++ ifelse.ts
@@ -1,1 +1,20 @@
 
+let a = 7;
+
+let b = 8;
+
+if(a==b){ //if
+
+  console.log("a and b are equal and same");
+}
+else{ //else 
+
+  console.log("a and b are not equal and same");
+  
+}  
+
+
+
+
+
+
