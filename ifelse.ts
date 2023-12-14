d05de998fe7fffd533f261205f3c5811bd6abb0e
Tharanishwaran <133676934+Tharanishwaran@users.noreply.github.com>
2023-12-14 20:50:48 +0530

    Update ifelse.ts

--- ifelse.ts
+++ ifelse.ts
@@ -13,6 +13,30 @@ else{ //else
   
 }  
 
+//else if ladder
+let score = 90;
+
+if (score >= 90) {
+
+  console.log("A");
+
+} 
+
+else if (score >= 80) {
+
+  console.log("B");
+
+} 
+
+else if (score >= 70) {
+
+  console.log("C");
+} 
+
+else {
+  
+  console.log("Fail");
+}
 
 
 
